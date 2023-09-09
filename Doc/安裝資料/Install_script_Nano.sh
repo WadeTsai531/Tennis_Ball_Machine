fca6f37d469bc731936a903f596431fd926b40f4
WadeTsai531 <tingwade531@gmail.com>
2023-09-09 13:14:07 +0800

    By Wade-PC

---
+++ Doc/安裝資料/Install_script_Nano.sh
@@ -0,0 +1,62 @@
+sudo apt-get update
+sudo apt-get upgrade
+cd ~
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.5.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.5.zip
+unzip opencv.zip unzip opencv_contrib.zip
+mv opencv-4.5.5 opencv mv opencv_contrib-4.5.5 opencv_contrib
+
+sudo apt-get install -y build-essential cmake git unzip pkg-config zlib1g-dev libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev
+sudo apt-get install -y libpng-dev libtiff-dev libglew-dev libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
+sudo apt-get install -y python-dev python-numpy python-pip python3-dev python3-numpy python3-pip
+sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
+sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev libxine2-dev
+sudo apt-get install -y gstreamer1.0-tools libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y libgstreamer-plugins-good1.0-dev
+sudo apt-get install -y libv4l-dev v4l-utils v4l2ucp qv4l2
+sudo apt-get install -y libtesseract-dev libxine2-dev libpostproc-dev
+sudo apt-get install -y libavresample-dev libvorbis-dev
+sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install -y liblapack-dev liblapacke-dev libeigen3-dev gfortran
+sudo apt-get install -y libhdf5-dev libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+
+cd ~/opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+-D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
+-D WITH_OPENCL=OFF \
+-D WITH_CUDA=ON \
+-D CUDA_ARCH_BIN=5.3 \
+-D CUDA_ARCH_PTX="" \
+-D WITH_CUDNN=ON \
+-D WITH_CUBLAS=ON \
+-D ENABLE_FAST_MATH=ON \
+-D CUDA_FAST_MATH=ON \
+-D OPENCV_DNN_CUDA=ON \
+-D ENABLE_NEON=ON \
+-D WITH_QT=OFF \
+-D WITH_OPENMP=ON \
+-D BUILD_TIFF=ON \
+-D WITH_FFMPEG=ON \
+-D WITH_GSTREAMER=ON \
+-D WITH_TBB=ON \
+-D BUILD_TBB=ON \
+-D BUILD_TESTS=OFF \
+-D WITH_EIGEN=ON \
+-D WITH_V4L=ON \
+-D WITH_LIBV4L=ON \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D INSTALL_C_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_EXAMPLES=OFF ..
+
